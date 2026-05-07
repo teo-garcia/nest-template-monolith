a946fffdde5258d81e090ce05a5b784b5b8ab65b
teo-garcia <juanmateogarcia96@gmail.com>
2026-05-06 21:43:15 -0500

    chore: remove stale eslint-disable simple-import-sort comments

--- src/shared/interceptors/index.ts
+++ src/shared/interceptors/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable simple-import-sort/exports */
-
 export { RequestIdInterceptor } from './request-id.interceptor'
-export { TransformInterceptor } from './transform.interceptor'
 export type { ApiResponse } from './transform.interceptor'
+export { TransformInterceptor } from './transform.interceptor'
